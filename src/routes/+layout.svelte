b6f71e9f65cbd203a184357035dc3f9e2046de7b
Lukas Lerche <lukas.lerche@tu-dortmund.de>
2023-03-21 12:41:29 +0100

    Base application layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-    import type { LayoutData } from './$types';
     import '@picocss/pico';
-    
-    export let data: LayoutData;
 </script>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,26 @@
+<script lang="ts">
+    let encoded: string;
+    let decoded: string;
+
+    function encode() : void {
+        
+    }
+
+    function decode() : void {
+        
+    }
+</script>
+
 <main class="container">
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <h1>URN Code 40 encoder/decoder</h1>
+
+
+    <input placeholder="Plain text" bind:value={encoded} />
+    <button on:click={encode}>Encode to URN Code 40</button>
+    <button on:click={decode}>Decode from URN Code 40</button>
+    <input placeholder="URN Code 40 encoded text" bind:value={decoded} />
+    <div style="color: red">Error: ...</div>
+    
+
 </main>
 
